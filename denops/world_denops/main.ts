372e7293f4e2a9ebc9c50c87c30b7cc455549df2
shinokun <hypatia.tile02021@gmail.com>
2026-02-05 08:55:41 +0900

    Add sum function.

--- denops/world_denops/main.ts
+++ denops/world_denops/main.ts
@@ -6,5 +6,16 @@ export async function main(denops: Denops): Promise<void> {
     async hello(): Promise<void> {
       await denops.cmd(`echomsg "Hello from world_denops!"`);
     },
+    async world(lhs, rhs): Promise<void> {
+      if (typeof lhs === "number" && typeof rhs === "number") {
+        await denops.cmd(`echomsg "The sum is: ${lhs + rhs}"`);
+        return;
+      }
+      if (typeof lhs === "string" && typeof rhs === "string") {
+        await denops.cmd(`echomsg "The concatenated string is: ${lhs + rhs}"`);
+        return;
+      }
+      await denops.cmd(`echomsg "Unsupported types"`);
+    },
   };
 }
